d574067c588a11b5f3b4f9957e76665741334aff
Darshan0312 <darshanbr081@gmail.com>
2024-04-15 09:28:34 +0530

    everything is updated

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -10,38 +10,38 @@ from typing import Any
 
 
 @ensure_annotations
-def read_yaml(path_to_yaml:Path) -> ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
-    
+
     Args:
-    path_to_yaml (str): path like input
-    
+        path_to_yaml (str): path like input
+
     Raises:
-    ValueError: if yaml file is empty
-    e: empty file
-    
+        ValueError: if yaml file is empty
+        e: empty file
+
     Returns:
-    ConfigBox: ConfigBox type
+        ConfigBox: ConfigBox type
     """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
-    expect BoxValueError:
+    except BoxValueError:
         raise ValueError("yaml file is empty")
-    expect Exception aa e:
-    raise e
-
+    except Exception as e:
+        raise e
+    
 
 
 @ensure_annotations
-def create_directories(path_to_directories: list,verbose=True):
+def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
-    
+
     Args:
-    path_to_directories (list): list of path of directories
-    ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
@@ -53,12 +53,14 @@ def create_directories(path_to_directories: list,verbose=True):
 @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
-    
+
     Args:
         path (Path): path of the file
-        
+
     Returns:
         str: size in KB
     """
     size_in_kb = round(os.path.getsize(path)/1024)
-    return f"{size_in_kb}KB"
+    return f"~ {size_in_kb} KB"
+
+    
--- template.py
+++ template.py
@@ -2,23 +2,23 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name="textSummarizer"
+project_name = "textSummarizer"
 
-list_of_files=[
-    ".github/workflow/.gitkeep",
+list_of_files = [
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__py",
+    f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constant/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -27,9 +27,8 @@ list_of_files=[
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py"
-]
 
+]
 
 
 for filepath in list_of_files:
@@ -38,13 +37,16 @@ for filepath in list_of_files:
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for thr file {filename}")
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
 
+    
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
+
